cbede84b68bb613599766a2d09ab3df5e8ffd688
Tomas Lycken <tomas.lycken@gmail.com>
2014-12-26 22:46:25 +0100

    Notebook with visualizations of behavior

--- test/visual.jl
+++ test/visual.jl
@@ -5,11 +5,15 @@ using Gadfly, Interpolations
 println("done!")
 const nx = 10
 xg = 1:nx
-xf = -.2nx:.1:4.3nx
+xf = -1.2nx:.1:3.3nx
 
 f1(x) = sin(2pi/nx * (x-1))
 
 y1 = map(f1,xg)
+
+p = plot()
+
+if true
 
 for (IT, EB) in (
         (Constant{OnCell}, ExtrapConstant),
@@ -24,14 +28,22 @@ for (IT, EB) in (
         (Quadratic{Flat,OnCell}, ExtrapReflect),
         (Quadratic{Free,OnCell}, ExtrapReflect),
         (Quadratic{Free,OnGrid}, ExtrapReflect),
+        (Quadratic{Periodic,OnGrid}, ExtrapPeriodic),
+        (Quadratic{Periodic,OnCell}, ExtrapPeriodic),
     )
     itp = Interpolation(y1, IT(), EB())
 
-    display(plot(
-        layer(x=xg,y=y1,Geom.point),
-        layer(x=xf,y=[itp[x] for x in xf],Geom.path),
-        Guide.title("$IT, $EB")
-    ))
+    stuff = Any[]
+
+    push!(stuff, layer(x=xg,y=y1,Geom.point,Theme(default_color=color("green"))))
+    push!(stuff, layer(x=xf,y=[itp[x] for x in xf],Geom.path,Theme(default_point_size=2px)))
+    push!(stuff, Guide.title("$IT, $EB"))
+    if boundarycondition(IT()) == Periodic()
+        push!(stuff, layer(x=xg,y=y1,Geom.point,Theme(default_color=color("green"))))
+    end
+    display(plot(stuff...))
+end
+
 end
 
 end
