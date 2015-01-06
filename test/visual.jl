974ce87c59f91432befda31efd3e73fc3af01ce1
Tomas Lycken <tomas.lycken@gmail.com>
2015-01-06 02:19:33 +0100

    Improve plotting examples' output

--- test/visual.jl
+++ test/visual.jl
@@ -1,8 +1,6 @@
 module VisualTests
 
-print("loading packages...")
 using Gadfly, Interpolations
-println("done!")
 const nx = 10
 xg = 1:nx
 xf = -1.2nx:.1:3.3nx
@@ -37,10 +35,8 @@ for (IT, EB) in (
 
     push!(stuff, layer(x=xg,y=y1,Geom.point,Theme(default_color=color("green"))))
     push!(stuff, layer(x=xf,y=[itp[x] for x in xf],Geom.path,Theme(default_point_size=2px)))
-    push!(stuff, Guide.title("$IT, $EB"))
-    if boundarycondition(IT()) == Periodic()
-        push!(stuff, layer(x=xg,y=y1,Geom.point,Theme(default_color=color("green"))))
-    end
+    title = "$(IT.name.name){$(join(map(t -> t.name.name, IT.parameters), ','))}, $(EB.name.name)"
+    push!(stuff, Guide.title(title))
     display(plot(stuff...))
 end
 
