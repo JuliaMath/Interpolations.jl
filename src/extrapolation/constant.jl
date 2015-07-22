6ff1157d532a5925729ec482834d7b77c9b48ecd
Tomas Lycken <tomas.lycken@jayway.com>
2015-07-22 23:19:53 +0200

    Simplify extrap indexing

--- src/extrapolation/constant.jl
+++ src/extrapolation/constant.jl
@@ -7,11 +7,11 @@ ConstantExtrapolation{T,ITP,IT,GT}(::Type{T}, N, itp::ITP, ::Type{IT}, ::Type{GT
 extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, ::Type{Flat}) =
     ConstantExtrapolation(T,N,itp,IT,GT)
 
-function extrap_prep{T,ITP,IT}(exp::Type{ConstantExtrapolation{T,1,ITP,IT,OnGrid}}, xs)
-    :(xs = clamp(xs, 1, size(exp,1)))
+function extrap_prep{T,ITP,IT}(exp::Type{ConstantExtrapolation{T,1,ITP,IT,OnGrid}}, x)
+    :(x = clamp(x, 1, size(exp,1)))
 end
-function extrap_prep{T,ITP,IT}(exp::Type{ConstantExtrapolation{T,1,ITP,IT,OnCell}}, xs)
-    :(xs = clamp(xs, .5, size(exp,1)+.5))
+function extrap_prep{T,ITP,IT}(exp::Type{ConstantExtrapolation{T,1,ITP,IT,OnCell}}, x)
+    :(x = clamp(x, .5, size(exp,1)+.5))
 end
 function extrap_prep{T,N,ITP,IT}(exp::Type{ConstantExtrapolation{T,N,ITP,IT,OnGrid}}, xs...) 
     :(@nexprs $N d->(@show xs[d] = clamp(xs[d], 1, size(exp,d))))
