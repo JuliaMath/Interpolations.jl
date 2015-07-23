7503dae32a69e10fff45b5a54caad3491b563962
Tomas Lycken <tomas.lycken@gmail.com>
2015-07-24 01:25:18 +0200

    Allow specifying TCoefs explicitly

--- src/b-splines/prefiltering.jl
+++ src/b-splines/prefiltering.jl
@@ -15,32 +15,34 @@ function padded_index{N,pad}(sz::NTuple{N,Int}, ::Val{pad})
     end
     ind,szpad
 end
-function copy_with_padding{IT<:DimSpec{InterpolationType}}(A, ::Type{IT})
+
+copy_with_padding{IT}(A, ::Type{IT}) = copy_with_padding(eltype(A), A, IT)
+function copy_with_padding{TCoefs,IT<:DimSpec{InterpolationType}}(::Type{TCoefs}, A, ::Type{IT})
     Pad = padding(IT)
     ind,sz = padded_index(size(A), Pad)
     if sz == size(A)
-        coefs = copy(A)
+        coefs = copy!(similar(A,TCoefs), A)
     else
-        coefs = zeros(eltype(A), sz...)
+        coefs = zeros(TCoefs, sz...)
         coefs[ind...] = A
     end
     coefs, Pad
 end
 
 prefilter!{TWeights, IT<:BSpline, GT<:GridType}(::Type{TWeights}, A, ::Type{IT}, ::Type{GT}) = A
-prefilter{TWeights, IT<:BSpline, GT<:GridType}(::Type{TWeights}, A, ::Type{IT}, ::Type{GT}) = prefilter!(TWeights, copy(A), IT, GT), Val{0}()
+prefilter{TWeights, TCoefs, IT<:BSpline, GT<:GridType}(::Type{TWeights}, ::Type{TCoefs}, A, ::Type{IT}, ::Type{GT}) = prefilter!(TWeights, copy!(similar(A,TCoefs), A), IT, GT), Val{0}()
 
-function prefilter{TWeights,TCoefs,N,IT<:Quadratic,GT<:GridType}(
-    ::Type{TWeights}, A::Array{TCoefs,N}, ::Type{BSpline{IT}}, ::Type{GT}
+function prefilter{TWeights,TCoefs,TSrc,N,IT<:Quadratic,GT<:GridType}(
+    ::Type{TWeights}, ::Type{TCoefs}, A::Array{TSrc,N}, ::Type{BSpline{IT}}, ::Type{GT}
     )
-    ret, Pad = copy_with_padding(A, BSpline{IT})
+    ret, Pad = copy_with_padding(TCoefs,A, BSpline{IT})
     prefilter!(TWeights, ret, BSpline{IT}, GT), Pad
 end
 
-function prefilter{TWeights,TCoefs,N,IT<:Tuple{Vararg{BSpline}},GT<:DimSpec{GridType}}(
-    ::Type{TWeights}, A::Array{TCoefs,N}, ::Type{IT}, ::Type{GT}
+function prefilter{TWeights,TCoefs,TSrc,N,IT<:Tuple{Vararg{BSpline}},GT<:DimSpec{GridType}}(
+    ::Type{TWeights}, ::Type{TCoefs}, A::Array{TSrc,N}, ::Type{IT}, ::Type{GT}
     )
-    ret, Pad = copy_with_padding(A, IT)
+    ret, Pad = copy_with_padding(TCoefs,A, IT)
     prefilter!(TWeights, ret, IT, GT), Pad
 end
 
