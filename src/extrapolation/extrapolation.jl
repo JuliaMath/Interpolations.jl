b0c616433ef4b1261925a246679665e802dcf4ce
Tomas Lycken <tomas.lycken@jayway.com>
2015-09-18 18:22:13 +0200

    Rename FilledInterpolation to FilledExtrapolation
    This change might have been better suited for its own PR, but I stumbled
    over it now and had to change some related things anyway (indexing into
    it, see next commit in #47).
    
    @timholy, please protest if you have any objections.

--- src/extrapolation/extrapolation.jl
+++ src/extrapolation/extrapolation.jl
@@ -1,5 +1,5 @@
 export Throw,
-       FilledInterpolation   # for direct control over typeof(fillvalue)
+       FilledExtrapolation   # for direct control over typeof(fillvalue)
 
 include("error.jl")
 
--- src/extrapolation/filled.jl
+++ src/extrapolation/filled.jl
@@ -1,25 +1,25 @@
 nindexes(N::Int) = N == 1 ? "1 index" : "$N indexes"
 
 
-type FilledInterpolation{T,N,ITP<:AbstractInterpolation,IT,GT,FT} <: AbstractExtrapolation{T,N,ITP,IT,GT}
+type FilledExtrapolation{T,N,ITP<:AbstractInterpolation,IT,GT,FT} <: AbstractExtrapolation{T,N,ITP,IT,GT}
     itp::ITP
     fillvalue::FT
 end
 @doc """
-`FilledInterpolation(itp, fillvalue)` creates an extrapolation object that returns the `fillvalue` any time the indexes in `itp[x1,x2,...]` are out-of-bounds.
+`FilledExtrapolation(itp, fillvalue)` creates an extrapolation object that returns the `fillvalue` any time the indexes in `itp[x1,x2,...]` are out-of-bounds.
 
 By comparison with `extrapolate`, this version lets you control the `fillvalue`'s type directly.  It's important for the `fillvalue` to be of the same type as returned by `itp[x1,x2,...]` for in-bounds regions for the index types you are using; otherwise, indexing will be type-unstable (and slow).
 """ ->
-function FilledInterpolation{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, fillvalue)
-    FilledInterpolation{T,N,typeof(itp),IT,GT,typeof(fillvalue)}(itp, fillvalue)
+function FilledExtrapolation{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, fillvalue)
+    FilledExtrapolation{T,N,typeof(itp),IT,GT,typeof(fillvalue)}(itp, fillvalue)
 end
 
 @doc """
 `extrapolate(itp, fillvalue)` creates an extrapolation object that returns the `fillvalue` any time the indexes in `itp[x1,x2,...]` are out-of-bounds.
 """ ->
-extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, fillvalue) = FilledInterpolation(itp, convert(eltype(itp), fillvalue))
+extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, fillvalue) = FilledExtrapolation(itp, convert(eltype(itp), fillvalue))
 
-@generated function getindex{T,N}(fitp::FilledInterpolation{T,N}, args::Number...)
+@generated function getindex{T,N}(fitp::FilledExtrapolation{T,N}, args::Number...)
     n = length(args)
     n == N || return error("Must index $(N)-dimensional interpolation objects with $(nindexes(N))")
     meta = Expr(:meta, :inline)
@@ -33,4 +33,4 @@ extrapolate{T,N,IT,GT}(itp::AbstractInterpolation{T,N,IT,GT}, fillvalue) = Fille
     end
 end
 
-getindex{T}(fitp::FilledInterpolation{T,1}, x::Number, y::Int) = y == 1 ? fitp[x] : throw(BoundsError())
+getindex{T}(fitp::FilledExtrapolation{T,1}, x::Number, y::Int) = y == 1 ? fitp[x] : throw(BoundsError())
