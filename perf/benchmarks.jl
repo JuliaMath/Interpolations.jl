1fbc136b27d82bee48ea128595e7cfc888f2e469
Tim Holy <tim.holy@gmail.com>
2017-04-04 08:01:12 -0500

    Focus benchmarks on interpolation, not range-construction

--- perf/benchmarks.jl
+++ perf/benchmarks.jl
@@ -10,17 +10,21 @@ end
 
 # To evaluate at fractional positions without any "unnecessary"
 # overhead, safer to use Base.Cartesian
-@generated function sumvalues{T,N}(itp::AbstractInterpolation{T,N})
-    n = Int(round(1000^(1/N)))
+@generated function sumvalues{T,N}(itp::AbstractInterpolation{T,N}, inds)
     quote
-        inds = indices(itp)
-        @nexprs $N d->inds_d = linspace(first(inds[d])+0.001, last(inds[d])-0.001, $n)
+        @nexprs $N d->inds_d = inds[d]
         s = zero(eltype(itp))
-        @nloops $N i d->inds_d begin
-            @inbounds s += @nref($N, itp, i)
+        @inbounds @nloops $N i d->inds_d begin
+            s += @nref($N, itp, i)
         end
         s
     end
+end
+
+function sumvalues_indices(itp)
+    inds = indices(itp)
+    n = Int(round(10^(3/ndims(itp))))
+    ntuple(d->collect(linspace(first(inds[d])+0.001, last(inds[d])-0.001, n)), ndims(itp))
 end
 
 strip_prefix(str) = replace(str, "Interpolations.", "")
@@ -51,8 +55,9 @@ for A in (collect(Float64, 1:3),
             suite["bsplines"][gstr][string(idstr, "_construct")] =
                 @benchmarkable interpolate($Ac, BSpline($D()), $GT())
             itp = interpolate(copy(A), BSpline(D()), GT())
+            inds = sumvalues_indices(itp)
             suite["bsplines"][gstr][string(idstr, "_use")] =
-                @benchmarkable sumvalues($itp)
+                @benchmarkable sumvalues($itp, $inds)
         end
     end
     # Quadratic
@@ -63,8 +68,9 @@ for A in (collect(Float64, 1:3),
         suite["bsplines"][gstr][string(idstr, "_construct")] =
             @benchmarkable interpolate($Ac, BSpline(Quadratic($BC())), $GT())
         itp = interpolate(copy(A), BSpline(Quadratic(BC())), GT())
+        inds = sumvalues_indices(itp)
         suite["bsplines"][gstr][string(idstr, "_use")] =
-            @benchmarkable sumvalues($itp)
+            @benchmarkable sumvalues($itp, $inds)
     end
     for BC in (InPlace,InPlaceQ)
         Ac = copy(A)
@@ -72,8 +78,9 @@ for A in (collect(Float64, 1:3),
         suite["bsplines"][gstr][string(idstr, "_construct")] =
             @benchmarkable interpolate!($Ac, BSpline(Quadratic($BC())), OnCell())
         itp = interpolate!(copy(A), BSpline(Quadratic(BC())), OnCell())
+        inds = sumvalues_indices(itp)
         suite["bsplines"][gstr][string(idstr, "_use")] =
-            @benchmarkable sumvalues($itp)
+            @benchmarkable sumvalues($itp, $inds)
     end
     # Cubic
     gstr = groupstr(Cubic)
@@ -83,8 +90,9 @@ for A in (collect(Float64, 1:3),
         suite["bsplines"][gstr][string(idstr, "_construct")] =
             @benchmarkable interpolate($Ac, BSpline(Cubic($BC())), $GT())
         itp = interpolate(copy(A), BSpline(Cubic(BC())), GT())
+        inds = sumvalues_indices(itp)
         suite["bsplines"][gstr][string(idstr, "_use")] =
-            @benchmarkable sumvalues($itp)
+            @benchmarkable sumvalues($itp, $inds)
     end
 end
 
@@ -93,6 +101,7 @@ paramspath = joinpath(dirname(@__FILE__), "params.jld")
 if isfile(paramspath)
     loadparams!(suite, BenchmarkTools.load(paramspath, "suite"), :evals);
 else
+    info("Tuning suite (this may take a while)")
     tune!(suite)
     BenchmarkTools.save(paramspath, "suite", params(suite));
 end
