200775f506e0f4ca8c51e991a5e317c06b0f9b9a
Felix Cremer <felix.cremer@uni-jena.de>
2018-12-16 13:40:12 +0100

    Setup makedocs() to use all .md files
    Use pages keyword of makedocs() to set the order of the docs.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,11 @@
 using Documenter, Interpolations
 makedocs(
 sitename="Interpolations.jl",
-modules=[Interpolations])
+modules=[Interpolations],
+pages=["Home" => "index.md",
+        "General usage" => "interpolations.md",
+        "Interpolation algorithms" => "control.md",
+        "Extrapolation" => "extrapolation.md",
+        "Convenience Construcors" => "convenience-construction.md",
+        "Library" => "api.md"]
+)
