87a715ea0fd4dbf368669d8532a0da9cf5f16d0e
Thibaut Lienart <tlienart@me.com>
2019-03-24 12:28:27 +1100

    typo fix in make.jl
    Convenience Construcors -> Convenience Constructors

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ pages=["Home" => "index.md",
         "General usage" => "interpolations.md",
         "Interpolation algorithms" => "control.md",
         "Extrapolation" => "extrapolation.md",
-        "Convenience Construcors" => "convenience-construction.md",
+        "Convenience Constructors" => "convenience-construction.md",
         "Library" => "api.md"]
 )
 
