06f9fc6dc7fcdaeeadf3b930a8b8385cb8566895
Tim Holy <tim.holy@gmail.com>
2019-05-08 07:08:02 -0500

    Generalize eltype pass-through to all AbstractFloat (fixes #308) (#317)

--- test/b-splines/runtests.jl
+++ test/b-splines/runtests.jl
@@ -6,4 +6,6 @@
     include("mixed.jl")
     include("multivalued.jl")
     include("non1.jl")
+
+    @test eltype(@inferred(interpolate(rand(Float16, 3, 3), BSpline(Linear())))) == Float16 # issue #308
 end
