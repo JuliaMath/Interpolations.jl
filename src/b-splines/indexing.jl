d1dca8787cd784ab22cc802e69c73455f41e629a
Tim Holy <tim.holy@gmail.com>
2020-04-13 07:42:46 -0500

    Fix typo
    Co-Authored-By: Anshul Singhvi <asinghvi17@simons-rock.edu>

--- src/b-splines/indexing.jl
+++ src/b-splines/indexing.jl
@@ -59,7 +59,7 @@ Compute `WeightedIndex` values for evaluation at the position `xs...`.
 typically `value_weights`, `gradient_weights`, and/or `hessian_weights`.
 `itpflags` and `nodes` can be obtained from `itpinfo(itp)...`.
 
-See the "developer documenation" for further information.
+See the "developer documentation" for further information.
 """
 @inline function weightedindexes(fs::F, itpflags::NTuple{N,Flag}, knots::NTuple{N,AbstractVector}, xs::NTuple{N,Number}) where {F,N}
     # parts = map((flag, knotvec, x)->weightedindex_parts(fs, flag, knotvec, x), itpflags, knots, xs)
