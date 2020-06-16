379a6b7c6ae6d9bcd73caa82bf894dd0d9c710a5
Miles Lucas <mdlucas@hawaii.edu>
2020-06-16 11:06:56 -0500

    remove redundant loops

--- src/convolutions/convolutions.jl
+++ src/convolutions/convolutions.jl
@@ -25,30 +25,30 @@ lanczos(T, x, a::Integer) = abs(x) < a ? T(sinc(x) * sinc(x / a)) : zero(T)
 
 interpolate(s::AbstractArray, it::Lanczos) = LanczosInterpolation(s, it.a, it)
 
-struct LanczosInterpolation{T, N, IT<:DimSpec{Lanczos}} <: AbstractInterpolation{T, N, IT}
-    data::Array{T,N}
+struct LanczosInterpolation{T, N, IT<:DimSpec{Lanczos},A<:AbstractArray{T, N}} <: AbstractInterpolation{T, N, IT}
+    data::A
     degree::Int
     it::IT
 end
 
-@generated function (itp::LanczosInterpolation{T, N})(pts...) where {T,N}
+@inline @generated function (itp::LanczosInterpolation{T, N})(pts::Vararg{<:Number, N}) where {T,N}
     quote
+        # if we can just index, let's
+        all(isinteger, pts) && return K[convert.(Int, pts)...]
+        @boundscheck checkbounds(K, pts...)
+        
         a = itp.degree
         K = itp.data
         w = s = 0
-        # get weighting
-        @nloops $N i (d -> -a+1:a+1) begin
-            q = 1
-            ls = @nexprs $N d -> q *= lanczos(T, i_d - pts[d] + floor(pts[d]), a)
-            w += q
-        end
-        # get value
-        @nloops $N i (d -> -a+1:a+1) (d -> idx_d = floor(Int, pts[d]) + i_d) begin
-            (@ncall $N checkbounds Bool K d -> idx_d) || continue
-            f = @nref $N K idx
-            ls = @nexprs $N d -> f *= lanczos(T, i_d - pts[d] + floor(pts[d]), a)
 
-            s += f
+        @inbounds @nloops $N i (d -> -a+1:a+1) (d -> idx_d = floor(Int, pts[d]) + i_d) begin
+            (@ncall $N checkbounds Bool K d -> idx_d) || continue
+            # get weighting
+            q = 1
+            @nexprs $N d -> q *= lanczos(T, i_d - pts[d] + floor(pts[d]), a)
+            w += q
+            # get value
+            s += (@nref $N K idx) * q
         end
         return s / w
     end
