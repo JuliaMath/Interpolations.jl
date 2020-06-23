405f2f55990c1ade56d5a97ec10fa17463f17be7
Miles Lucas <mdlucas@hawaii.edu>
2020-06-23 13:48:27 -0500

    speed improvements

--- src/lanczos/lanczos.jl
+++ src/lanczos/lanczos.jl
@@ -22,14 +22,9 @@ end
 
 Lanczos(a=4) = Lanczos(a, a)
 
-
 """
-    lanczos(x, a, n=a)
-
-Implementation of the [Lanczos kernel](https://en.wikipedia.org/wiki/Lanczos_resampling)
+    LanczosInterpolation
 """
-lanczos(x::T, a::Integer, n=a) where {T} = abs(x) < n ? T(sinc(x) * sinc(x / a)) : zero(T)
-
 struct LanczosInterpolation{T,N,IT <: DimSpec{Lanczos},A <: AbstractArray{T,N},P <: Tuple{Vararg{AbstractArray,N}}} <: AbstractInterpolation{T,N,IT}
     coefs::A
     parentaxes::P
@@ -46,18 +41,18 @@ lbounds(itp::LanczosInterpolation) = map(first, itp.parentaxes)
 ubounds(itp::LanczosInterpolation) = map(last, itp.parentaxes)
 
 function interpolate(A::AbstractArray{T}, it::Lanczos) where T
-    Apad = copy_with_padding(T, A, it)
+    Apad = copy_with_padding(float(T), A, it)
     return LanczosInterpolation(Apad, axes(A), it)
 end
 
 @inline function (itp::LanczosInterpolation{T,N})(x::Vararg{<:Number,N}) where {T,N}
     @boundscheck (checkbounds(Bool, itp, x...) || Base.throw_boundserror(itp, x))
     wis = weightedindexes((value_weights,), itpinfo(itp)..., x)
-    coefficients(itp)[wis...]
+    itp.coefs[wis...]
 end
 
-function weightedindex_parts(fs::F, it::Lanczos, ax::AbstractUnitRange{<:Integer}, x) where F
-    pos, δx = positions(it, ax,  x)
+function weightedindex_parts(fs, it::Lanczos, ax::AbstractUnitRange{<:Integer}, x)
+    pos, δx = positions(it, ax, x)
     (position = pos, coefs = fmap(fs, it, δx))
 end
 
@@ -67,16 +62,17 @@ function positions(it::Lanczos, ax, x)
     fast_trunc(Int, xf) - it.n + 1, δx
 end
 
-function value_weights(it::Lanczos, δx::T) where T
-    # LUTs
-    it.a == it.n == 4 && return _lanczos4(δx)
-
+function value_weights(it::Lanczos, δx::S) where S
+    T = float(S)
     # short-circuit if integral
     isinteger(δx) && return ntuple(i -> i == it.n - δx ? one(T) : zero(T), 2it.n)
 
-    idxs = -it.n + 1:it.n
-    cs = @. lanczos(idxs + δx, it.a, it.n)
-    return Tuple(cs ./ sum(cs))
+    # LUTs
+    it.a == it.n == 4 && return _lanczos4(δx)
+
+    cs = ntuple(i -> lanczos(it.n - i + δx, it.a, it.n), 2it.n)
+    normed_cs = ntuple(i -> cs[i] / sum(cs), length(cs))
+    return normed_cs
 end
 
 function padded_axis(ax::AbstractUnitRange, it::Lanczos)
@@ -85,16 +81,25 @@ end
 
 # precise implementations for fast evaluation of common kernels
 
+"""
+    lanczos(x, a, n=a)
+
+Implementation of the [Lanczos kernel](https://en.wikipedia.org/wiki/Lanczos_resampling)
+"""
+lanczos(x::T, a::Integer, n=a) where {T} = abs(x) < n ? T(sinc(x) * sinc(x / a)) : zero(T)
+
+
 const s45 = 0.70710678118654752440084436210485
 const l4_2d_cs = SA[1 0; -s45 -s45; 0 1; s45 -s45; -1 0; s45 s45; 0 -1; -s45 s45]
 
-function _lanczos4(δx::T) where T
-    isinteger(δx) && return ntuple(i -> i == 4 - δx ? one(T) : zero(T), 8)
-    y0 = -(δx + 3) * π/4
+function _lanczos4(δx)
+    p_4 = π / 4
+    y0 = -(δx + 3) * p_4
     s0, c0 = sincos(y0)
     cs = ntuple(8) do i
-        y = -(δx + 3 - i) * π / 4
+        y = (δx + 4 - i) * p_4
         (l4_2d_cs[i, 1] * s0 + l4_2d_cs[i, 2] * c0) / y^2
     end
-    return cs ./ sum(cs)
+    normed_cs = ntuple(i -> cs[i] / sum(cs), 8)
+    return normed_cs
 end
