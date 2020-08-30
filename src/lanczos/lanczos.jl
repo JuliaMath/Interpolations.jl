19d6ac0c6bb50e1ac04f4f5b906945a360823ec8
Mark Kittisopikul <markkitt@gmail.com>
2020-08-30 01:22:22 -0400

    Split lanczos implementation into direct and opencv based versions

--- src/lanczos/lanczos.jl
+++ src/lanczos/lanczos.jl
@@ -1,16 +1,18 @@
 using Base.Cartesian
 using StaticArrays
 
-export Lanczos
+export Lanczos, Lanczos4OpenCV
+
+abstract type AbstractLanczos <: InterpolationType end
 
 """
     Lanczos{N}(a=4)
 
 Lanczos resampling via a kernel with scale parameter `a` and support over `N` neighbors.
 
-This form of interpolation is merely the discrete convolution of the samples with a Lanczos kernel of size `a`. The size is directly related to how "far" the interpolation will reach for information, and has `O(N^2)` impact on runtime. A default value of 4 matches the OpenCV implementation `lanczos4`.
+This form of interpolation is merely the discrete convolution of the samples with a Lanczos kernel of size `a`. The size is directly related to how "far" the interpolation will reach for information, and has `O(N^2)` impact on runtime. An alternative implementation matching `lanczos4` from OpenCV is available as Lanczos4OpenCV.
 """
-struct Lanczos{N} <: InterpolationType
+struct Lanczos{N} <: AbstractLanczos
     a::Int
     
     function Lanczos{N}(a) where N
@@ -24,7 +26,7 @@ Lanczos(a=4) = Lanczos{a}(a)
 """
     LanczosInterpolation
 """
-struct LanczosInterpolation{T,N,IT <: DimSpec{Lanczos},A <: AbstractArray{T,N},P <: Tuple{Vararg{AbstractArray,N}}} <: AbstractInterpolation{T,N,IT}
+struct LanczosInterpolation{T,N,IT <: DimSpec{AbstractLanczos},A <: AbstractArray{T,N},P <: Tuple{Vararg{AbstractArray,N}}} <: AbstractInterpolation{T,N,IT}
     coefs::A
     parentaxes::P
     it::IT
@@ -41,7 +43,7 @@ axes(itp::LanczosInterpolation) = itp.parentaxes
 lbounds(itp::LanczosInterpolation) = map(first, itp.parentaxes)
 ubounds(itp::LanczosInterpolation) = map(last, itp.parentaxes)
 
-function interpolate(A::AbstractArray{T}, it::Lanczos) where T
+function interpolate(A::AbstractArray{T}, it::AbstractLanczos) where T
     Apad = copy_with_padding(float(T), A, it)
     return LanczosInterpolation(Apad, axes(A), it)
 end
@@ -52,12 +54,12 @@ end
     itp.coefs[wis...]
 end
 
-function weightedindex_parts(fs, it::Lanczos, ax::AbstractUnitRange{<:Integer}, x)
+function weightedindex_parts(fs, it::AbstractLanczos, ax::AbstractUnitRange{<:Integer}, x)
     pos, δx = positions(it, ax, x)
     (position = pos, coefs = fmap(fs, it, δx))
 end
 
-function positions(it::Lanczos, ax, x)
+function positions(it::AbstractLanczos, ax, x)
     xf = floorbounds(x, ax)
     δx = x - xf
     fast_trunc(Int, xf) - degree(it) + 1, δx
@@ -69,14 +71,16 @@ function value_weights(it::Lanczos, δx::S) where S
     isinteger(δx) && return ntuple(i->convert(float(S), i == N - δx), Val(2N))
 
     # LUTs
-    it.a === N === 4 && return _lanczos4(δx)
+    #it.a === N === 4 && return _lanczos4(δx)
 
     cs = ntuple(i -> lanczos(N - i + δx, it.a, N), Val(2N))
-    normed_cs = ntuple(i -> cs[i] / sum(cs), Val(length(cs)))
+    sum_cs = sum(cs)
+    @info sum_cs
+    normed_cs = ntuple(i -> cs[i] / sum_cs, Val(length(cs)))
     return normed_cs
 end
 
-function padded_axis(ax::AbstractUnitRange, it::Lanczos)
+function padded_axis(ax::AbstractUnitRange, it::AbstractLanczos)
     N = degree(it)
     return first(ax) - N + 1:last(ax) + N
 end
@@ -90,12 +94,24 @@ Implementation of the [Lanczos kernel](https://en.wikipedia.org/wiki/Lanczos_res
 """
 lanczos(x::T, a::Integer, n=a) where {T} = abs(x) < n ? T(sinc(x) * sinc(x / a)) : zero(T)
 
+"""
+    Lanczos4OpenCV()
+
+Alternative implementation of Lanczos resampling using algorithm `lanczos4` function of OpenCV:
+https://github.com/opencv/opencv/blob/de15636724967faf62c2d1bce26f4335e4b359e5/modules/imgproc/src/resize.cpp#L917-L946
+"""
+struct Lanczos4OpenCV <: AbstractLanczos
+end
+
+degree(::Lanczos4OpenCV) = 4
+
+value_weights(::Lanczos4OpenCV, δx::S) where S = ifelse(isinteger(δx),ntuple(i->convert(float(S), i == 4 - δx), Val(8)) ,_lanczos4_opencv(δx))
 
 const s45 = 0.70710678118654752440084436210485
 const l4_2d_cs = SA[1 0; -s45 -s45; 0 1; s45 -s45; -1 0; s45 s45; 0 -1; -s45 s45]
 
 
-function _lanczos4(δx)
+function _lanczos4_opencv(δx)
     p_4 = π / 4
     y0 = -(δx + 3) * p_4
     s0, c0 = sincos(y0)
@@ -103,6 +119,7 @@ function _lanczos4(δx)
         y = (δx + 4 - i) * p_4
         (l4_2d_cs[i, 1] * s0 + l4_2d_cs[i, 2] * c0) / y^2
     end
-    normed_cs = ntuple(i -> cs[i] / sum(cs), Val(8))
+    sum_cs = sum(cs)
+    normed_cs = ntuple(i -> cs[i] / sum_cs, Val(8))
     return normed_cs
 end
