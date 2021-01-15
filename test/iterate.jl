1895052af1f007df171dc7bde1959274b1d21af6
Alexius Wadell <awadell@gmail.com>
2021-01-15 13:40:21 -0800

    Expand @test_knots + docs and add extrapolation tests
    Moved macro to the top of the test file and expanded docs
    - Added type stability check (Marked as broken for the moment)
    - Escape eval of reference iterator when getting knotType -> Otherwise
      can get "Undefined Var" errors
    - Build expected IteratorSize based on number of reference dimensions
    
    Added knot_ref helper function to build reference iterators
    - Quick but dumb (Uses takewhile/dropwhile) to construct reference
      iterators
    
    Added more test sets for knotsbetween
    - 1D Extrapolation (Non-Repeating)
    - 1D Reflect Extrapolation
    - 1D Periodic Extrapolation

--- test/iterate.jl
+++ test/iterate.jl
@@ -2,6 +2,143 @@ using Test
 using Interpolations
 using Interpolations: KnotRange
 
+"""
+    @test_knots kiter T expect
+
+Generate test sets for testing the knot iterator `kiter`. Following checks are
+performed:
+- `typeof(kiter)` is `T` if 1D or `ProductIterator` wrapping `T` if ND
+- `IteratorEltype` and `eltype` behavior matches expected
+- `IteratorSize`, `length` and `size` are matches expected
+- Iterated contents match (Checks all if bounded, otherwise the first 100)
+"""
+macro test_knots(itersym, type, expect...)
+    # 1D vs ND Checks
+    if length(expect) > 1
+        refExpr = :( Iterators.product($(expect...)) )
+        knotTypeExpr = :( Tuple{ eltype.(($(expect...),))... } )
+        typecheckExpr = quote
+            @test typeof($itersym) <: Iterators.ProductIterator
+            @test typeof($itersym.iterators) <: Tuple{$type, $type}
+        end
+        sizecheckExpr = :( @test size($itersym) == size(ref) )
+
+        # Expect HasShape{N} for ND Iterators
+        IteratorSizeExpr = :( Base.HasShape{$(length(expect))}() )
+    else
+        refExpr = expect[1]
+        knotTypeExpr = :( eltype($refExpr) )
+        typecheckExpr = :( @test (<:)(typeof($itersym), $type) )
+
+        # Check size using length to avoid issues with reference not defining a
+        # suitable size method
+        sizecheckExpr = :( @test size($itersym) == (length(ref),) )
+
+        # Expect a IteratorSize of HasLength for 1D iterators
+        IteratorSizeExpr = :( Base.HasLength() )
+    end
+
+    # Build test_knots tests
+    quote
+        local $itersym = $(esc(itersym))    # Local knot iterator for testing
+        knotType = $(esc(knotTypeExpr))     # Insert Expr to get knot eltype
+        ref = $(esc(refExpr))               # Build reference iterator
+
+        # If the reference size is "SizeUnknown" treat as IsInfinite
+        # Some iterators are quick to assume SizeUnknown
+        isBounded = Base.IteratorSize(ref) != Base.IsInfinite() &&
+                    Base.IteratorSize(ref) != Base.SizeUnknown()
+
+        # Insert Type checks defined earlier
+        @testset "typechecks" begin $typecheckExpr end
+
+        # Check Eltype matches reference iterator
+        # Relax type check (ie. <: instead of ==) as some iterators are quick to
+        # assume "Any" -> Prevents errors caused by ref's type being off
+        # This is okay, as we check that collected eltypes match exactly later
+        @testset "eltype checks" begin
+            @test Base.IteratorEltype($itersym) == Base.HasEltype()
+            @test eltype($itersym) <: knotType
+        end
+
+        # Check that IteratorSize matches reference and iff bounded check that
+        # length matches as well
+        # Size check is defined by $sizecheckExpr to avoid issues with Undefined
+        # size methods for 1D reference iterators
+        @testset "IteratorSize, length and size checks" begin
+            if isBounded
+                @test Base.IteratorSize($itersym) == $IteratorSizeExpr
+                @test length($itersym) == length(ref)
+                $sizecheckExpr
+            else
+                @test Base.IteratorSize($itersym) == Base.IsInfinite()
+            end
+        end
+
+        # Check that the contents of the knot iterator matches the reference
+        # iterator
+        @testset "check contents" begin
+            if isBounded
+                kiter = collect($itersym)
+                kref = collect(ref)
+            else
+                kiter = collect(Iterators.take($itersym, 100))
+                kref = collect(Iterators.take(ref, 100))
+            end
+            # Test Iterator Results
+            @test typeof(kiter) <: AbstractArray
+            @test eltype(kiter) == eltype(kref)
+            @test length(kiter) == length(kref)
+            @test size(kiter) == size(kref)
+            @test map((x,y) -> all(x .≈ y), kiter, kref) |> all
+        end
+
+        # Type stability -> Check that methods are type suitable
+        @testset "type stability" begin
+            @test_skip item, next = @inferred iterate($itersym)
+            @test_skip @inferred iterate($itersym, next)
+        end
+    end
+end
+
+"""
+    knot_ref(seq, etp)
+    knot_ref(seq, etp; start, stop)
+
+Dumb but correct construction of a reference knot iterator for a boundary condition
+of etp.
+
+If start and/or stop is specified, will wrap an dropwhile / takewhile around
+the resulting iterator
+"""
+knot_ref(seq, etp; start=nothing, stop=nothing) = knot_ref(seq, etp, start, stop)
+function knot_ref(seq, etp, start::Number, ::Nothing)
+    # Drop items less than start -> Dispatch to generate sequence
+    Iterators.dropwhile(<=(start), knot_ref(seq, etp))
+end
+function knot_ref(seq, etp, start, stop::Number)
+    # Drop items after stop + collect items (Better IteratorSize /Eltype results)
+    # Dispatch to handle remaining arguments
+    Iterators.takewhile(<(stop), knot_ref(seq, etp, start, nothing)) |> collect
+end
+
+knot_ref(seq, etp, ::Nothing, ::Nothing) = knot_ref(seq, etp)
+
+knot_ref(seq, ::Interpolations.BoundaryCondition) = seq
+
+function knot_ref(seq, ::Periodic)
+    Δ = diff(seq)
+    iter = Iterators.flatten((first(seq), Iterators.cycle(Δ)))
+    Iterators.accumulate(+, iter)
+end
+
+function knot_ref(seq, ::Reflect)
+    Δ = diff(seq)
+    Δ = vcat(Δ, reverse(Δ))
+    iter = Iterators.flatten((first(seq), Iterators.cycle(Δ)))
+    Iterators.accumulate(+, iter)
+end
+
 # Unit Tests for Base.IteratorEltype and Base.IteratorSize methods (Type Info Only)
 @testset "iterate - interface" begin
     import Interpolations.KnotIterator
@@ -212,59 +349,6 @@ end
     @test etp.(k) ≈ vcat(x, reverse(x[1:end - 1]), x[2]).^2
 end
 
-# Test Cases for iteration over the knots of 2D interpolant
-#   itersym Iterator under tests (ie. iter = knots(itp))
-#   type    The iterator type wrapped by Iterators.ProductIterator
-#   expect  One or more iterables for the knots along each dimension
-macro test_knots(itersym, type, expect...)
-
-    # 1D vs ND Checks
-    if length(expect) > 1
-        refExpr = :( Iterators.product($(expect...)) )
-        knotType = eval.(expect) .|> eltype |> x -> Tuple{x...}
-        typecheckExpr = quote
-            @test typeof($itersym) <: Iterators.ProductIterator
-            @test typeof($itersym.iterators) <: Tuple{$type, $type}
-        end
-    else
-        refExpr = :( Iterators.flatten($(expect[1])) )
-        knotType = eval(expect[1]) |> eltype
-        typecheckExpr = :( @test typeof($itersym) <: $type{$knotType} )
-    end
-
-    # Assemble component tests into complete testset
-    quote
-        local $itersym = $(esc(itersym))
-        ref = $refExpr
-        @testset "typechecks" begin $typecheckExpr end
-        @testset "eltype checks" begin
-            @test Base.IteratorEltype($itersym) == Base.HasEltype()
-            @test eltype($itersym) == $knotType
-        end
-        @testset "IteratorSize, length and size checks" begin
-            @test Base.IteratorSize($itersym) == Base.IteratorSize(ref)
-            if Base.IteratorSize(ref) != Base.IsInfinite()
-                @test length($itersym) == length(ref)
-            end
-        end
-        @testset "check contents" begin
-            if Base.IteratorSize(ref) != Base.IsInfinite()
-                kiter = collect($itersym)
-                kref = collect(ref)
-            else
-                kiter = collect(Iterators.take($itersym, 10))
-                kref = collect(Iterators.tail($itersym, 10))
-            end
-            # Test Iterator Results
-            @test typeof(kiter) <: AbstractArray
-            @test eltype(kiter) == $knotType
-            @test length(kiter) == length(kref)
-            @test size(kiter) == size(kref)
-            @test kiter == kref
-        end
-    end
-end
-
 # Unit tests for 2D iteration with directional boundary conditions that are
 # bounded (ie. knots do not repeat indefinitely)
 @testset "2D - iteration - bounded - $bc" for bc ∈ [Line(), (Throw(), Line())]
@@ -317,7 +401,6 @@ end
 
 @testset "knotsbetween - interpolate - 1D" begin
     itp = interpolate(rand(10), BSpline(Linear()))
-
     @testset "start and stop" begin
         krange = knotsbetween(itp; start=2.1, stop=6.2)
         @test_knots krange KnotRange 3:6
@@ -352,3 +435,78 @@ end
     end
 end
 
+@testset "knotsbetween - extrapolate - 1D - $etp" for etp ∈ [Throw(), Flat(), Line()]
+    x = [1.0, 1.5, 1.75, 2.0]
+    etp = LinearInterpolation(x, x.^2, extrapolation_bc=etp)
+    @testset "start and stop" begin
+        krange = knotsbetween(etp; start=0.0, stop=4.2)
+        @test_knots krange KnotRange [1.0, 1.5, 1.75, 2.0]
+    end
+    @testset "start" begin
+        krange = knotsbetween(etp; start=1.2)
+        @test_knots krange KnotRange [1.5, 1.75, 2.0]
+    end
+    @testset "stop" begin
+        krange = knotsbetween(etp; start=1.2)
+        @test_knots krange KnotRange [1.5, 1.75, 2.0]
+    end
+    @testset "empty iterator" begin
+        krange = knotsbetween(etp; start=3)
+        @test_knots krange KnotRange []
+
+        krange = knotsbetween(etp; stop=1.0)
+        @test_knots krange KnotRange []
+    end
+end
+
+@testset "knotsbetween - extrapolate - 1D - Periodic" begin
+    x = [1.0, 1.5, 1.75, 2.0]
+    etp = LinearInterpolation(x, x.^2, extrapolation_bc=Periodic())
+    @testset "start and stop" begin
+        krange = knotsbetween(etp; start=0.0, stop=4.2)
+        expect = knot_ref([0.5, 0.75, 1.0, 1.5], Periodic(); stop=4.2)
+        @test_knots krange KnotRange expect
+        @test all(0.0 .< collect(krange) .< 4.2)
+    end
+    @testset "start" begin
+        krange = knotsbetween(etp; start=3.2)
+        expect = knot_ref([3.5, 3.75, 4.0, 4.5], Periodic())
+        @test_knots krange KnotRange expect
+    end
+    @testset "stop" begin
+        krange = knotsbetween(etp; stop=4.8)
+        expect = knot_ref([1.0, 1.5, 1.75, 2.0], Periodic(); stop=4.8)
+        @test_knots krange KnotRange expect
+        @test all(collect(krange) .< 4.8)
+    end
+    @testset "empty iterator" begin
+        krange = knotsbetween(etp; start=1, stop=1)
+        @test_knots krange KnotRange Float64[]
+    end
+end
+
+@testset "knotsbetween - extrapolate - 1D - Reflect" begin
+    x = [1.0, 1.5, 1.75, 2.0]
+    etp = LinearInterpolation(x, x.^2, extrapolation_bc=Reflect())
+    @testset "start and stop" begin
+        krange = knotsbetween(etp; start=0.0, stop=4.2)
+        expect = knot_ref([0.5, 0.75, 1.0, 1.5], Reflect(); stop=4.2)
+        @test_knots krange KnotRange expect
+        @test all(0.0 .< collect(krange) .< 4.2)
+    end
+    @testset "start" begin
+        krange = knotsbetween(etp; start=3.2)
+        expect = knot_ref([3.5, 3.75, 4.0, 4.5], Reflect())
+        @test_knots krange KnotRange expect
+    end
+    @testset "stop" begin
+        krange = knotsbetween(etp; stop=4.8)
+        expect = knot_ref([1.0, 1.5, 1.75, 2.0], Reflect(); stop=4.8)
+        @test_knots krange KnotRange expect
+        @test all(collect(krange) .< 4.8)
+    end
+    @testset "empty iterator" begin
+        krange = knotsbetween(etp; start=1, stop=1)
+        @test_knots krange KnotRange []
+    end
+end
