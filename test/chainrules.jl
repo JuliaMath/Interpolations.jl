030a5019630b8f4ebc29f135a660d87abc9630ce
Paresh Mathur <paresh2047@gmail.com>
2021-04-17 11:16:28 +0200

    make testset and add a 2D example

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -1,10 +1,19 @@
 using Zygote
+@testset "ChainRulesCore" begin
+    # 1D example
+    x = 1:10
+    y = sin.(x)
 
-x = collect(1:10)
-y = sin.(x)
+    # Simple test that rrules give equivalent results to internal gradient function
 
-# Simple test that rrules give equivalent results to internal gradient function
+    itp = interpolate(y,BSpline(Linear()))
 
-itp = interpolate(y,BSpline(Linear()))
+    @test Zygote.gradient(itp, 1)[1] == Interpolations.gradient(itp, 1)
 
-@test Zygote.gradient(itp, 1)[1] == Interpolations.gradient(itp, 1)
+    # 2D example
+    x2 = [i*j for i=1:5, j=1:5]
+    y2 = sin.(x2)
+
+    itp2 = interpolate(y2, BSpline(Cubic(Line(OnGrid()))))
+    @test Zygote.gradient(itp2,1,2)[1] == Interpolations.gradient(itp2,1,2)
+end
