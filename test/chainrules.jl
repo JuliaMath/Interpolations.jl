f2f216365f97642f1b2158218bc8e0f826e4c0fa
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2021-06-29 06:22:21 -0400

    Make tests version dependent, test 1.0 and nightly

--- test/chainrules.jl
+++ test/chainrules.jl
@@ -8,12 +8,21 @@ using Zygote
 
     itp = interpolate(y,BSpline(Linear()))
 
-    @test Zygote.gradient(itp, 1)[1] == Interpolations.gradient(itp, 1)
+    if VERSION ≥ v"1.3"
+        @test Zygote.gradient(itp, 1)[1] == Interpolations.gradient(itp, 1)
+    else
+        @test_skip Zygote.gradient(itp, 1)[1] == Interpolations.gradient(itp, 1)
+    end
 
     # 2D example
     x2 = [i*j for i=1:5, j=1:5]
     y2 = sin.(x2)
 
     itp2 = interpolate(y2, BSpline(Cubic(Line(OnGrid()))))
-    @test Zygote.gradient(itp2,1,2)[1] == Interpolations.gradient(itp2,1,2)
+
+    if VERSION ≥ v"1.3"
+        @test Zygote.gradient(itp2,1,2)[1] == Interpolations.gradient(itp2,1,2)
+    else
+        @test_skip Zygote.gradient(itp2,1,2)[1] == Interpolations.gradient(itp2,1,2)
+    end
 end
