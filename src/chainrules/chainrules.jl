1761af90069a71611072c4e530137a058761c638
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-10-17 09:40:45 -0400

    splat arguments in rrule

--- src/chainrules/chainrules.jl
+++ src/chainrules/chainrules.jl
@@ -1,12 +1,16 @@
 export rrule
 """
     ChainRulesCore.rrule(itp::AbstractInterpolation, x...)
+
 ChainRulesCore.jl `rrule` for integration with automatic differentiation libraries.
+Note that it gives the gradient only with respect to the evaluation point `x`,
+and not the data inside `itp`.
 """
 function ChainRulesCore.rrule(itp::AbstractInterpolation, x...)
     y = itp(x...)
-    function pullback(Δy)
-        (ChainRulesCore.NoTangent(), Δy * Interpolations.gradient(itp, x...))
+    function interpolate_pullback(Δy)
+        nope = ChainRulesCore.@not_implemented "`Interpolations.gradient` does not calculate a gradient with respect to the original data, only the evaluation point"
+        (nope, (Δy * Interpolations.gradient(itp, x...))...)
     end
-    y, pullback
+    y, interpolate_pullback
 end
