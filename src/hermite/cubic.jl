08fab1f211cc9ff34ec5362f9a9d954f653305ae
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-03-28 10:21:44 -0400

    Update src/hermite/cubic.jl

--- src/hermite/cubic.jl
+++ src/hermite/cubic.jl
@@ -1,3 +1,8 @@
+"""
+     CubicHermite
+     
+This type is purposely left undocumented since the interface is expected to radically change in order to make it conform to the `AbstractInterpolation` interface. Consider this API to be highly unstable and non-public, and that breaking changes to this code could be made in a point release.
+"""
 struct CubicHermite
     xs::Vector{Float64}
     ys::Vector{Float64}
