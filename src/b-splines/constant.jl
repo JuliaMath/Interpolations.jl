ba65949a0f0f7e9bebb38883a9d8b79180fa606d
judober <16521254+judober@users.noreply.github.com>
2022-08-15 23:28:55 +0200

    remove brackets

--- src/b-splines/constant.jl
+++ src/b-splines/constant.jl
@@ -56,9 +56,9 @@ end
 Constant b-splines are *nearest-neighbor* interpolations, and effectively
 return `A[round(Int,x)]` when interpolating.
 
-`Constant{Previous}()` interpolates to the previous value thus equivalent
+`Constant{Previous}` interpolates to the previous value thus equivalent
 to `A[floor(Int,x)]`.
-`Constant{Next}()` interpolates to the next value thus equivalent
+`Constant{Next}` interpolates to the next value thus equivalent
 to `A[ceil(Int,x)]`.
 """
 Constant
