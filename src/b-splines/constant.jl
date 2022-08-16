1de4c3f4faf55610b4fc3bbbd10651d8589dc05b
judober <16521254+judober@users.noreply.github.com>
2022-08-16 10:42:01 +0200

    Added note for const and scaling

--- src/b-splines/constant.jl
+++ src/b-splines/constant.jl
@@ -55,6 +55,7 @@ end
 """
 Constant b-splines are *nearest-neighbor* interpolations, and effectively
 return `A[round(Int,x)]` when interpolating without scaling.
+Scaling can lead to inaccurate position of the *neighbors* due to limited numerical precision.
 
 `Constant{Previous}` interpolates to the previous value and is thus equivalent
 to `A[floor(Int,x)]` without scaling.
