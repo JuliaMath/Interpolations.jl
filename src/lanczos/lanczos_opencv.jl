a127d7d39129ee648c7676d3c736b0700aa92a12
Ben Arthur <bjarthur70@gmail.com>
2025-10-24 17:13:47 -0400

    reduced precision for lanczos_opencv

--- src/lanczos/lanczos_opencv.jl
+++ src/lanczos/lanczos_opencv.jl
@@ -65,20 +65,21 @@ const s45 = 0.70710678118654752440084436210485
 const l4_2d_cs = SA[1 0; -s45 -s45; 0 1; s45 -s45; -1 0; s45 s45; 0 -1; -s45 s45]
 
 
-function _lanczos4_opencv(δx)
-    p_4 = π / 4
+function _lanczos4_opencv(δx::T) where T
+    F = float(T)
+    p_4 = π / F(4)
     y0 = -(δx + 3) * p_4
-    s0, c0 = sincos(y0)
+    s0::F, c0::F = sincos(y0)
     cs = ntuple(8) do i
         y = (δx + 4 - i) * p_4
         # Improve precision of Lanczos OpenCV4 #451, avoid NaN
         if iszero(y)
-            y = eps(oneunit(y))/8
+            y = eps(oneunit(y))/F(8)
         end
         # Numerator is the sin subtraction identity
         # It is equivalent to the following
         # f(δx,i) = sin( π/4*( 5*(i-1)-δx-3 ) )
-        (l4_2d_cs[i, 1] * s0 + l4_2d_cs[i, 2] * c0) / y^2
+        (F(l4_2d_cs[i, 1]) * s0 + F(l4_2d_cs[i, 2]) * c0) / y^2
     end
     sum_cs = sum(cs)
     normed_cs = ntuple(i -> cs[i] / sum_cs, Val(8))
